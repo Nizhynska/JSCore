23fa8c994a64c04ba8e4101c84249fa682ff26fa
Polina <polinanizhynska@gmail.com>
2020-06-05 20:42:31 +0300

    fix: change folder name

--- Homework5/task7.js
+++
@@ -1,9 +0,0 @@
-let main = document.createElement("main");
-document.body.append(main);
-main.classList.add("mainClass", "check","item");
-let myDiv = document.createElement("div");
-main.append(myDiv);
-myDiv.id = "myDiv";
-let par = document.createElement("p");
-myDiv.append(par);
-par.innerHTML = "First paragraph";
